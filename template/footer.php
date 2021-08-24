dc91f581add4b0f72fa2db6c3362c293d40871dc
rogergomez <39565229+rogergomez@users.noreply.github.com>
2021-08-24 17:04:44 -0500

    Add files via upload

---
+++ template/footer.php
@@ -0,0 +1,13 @@
+<footer class="container-fluid bg-dark fixed-bottom">
+        <div class="row">
+            <div class="col-md text-light text-center py-3">
+                Desarrollado por Roger Gómez
+            </div>
+        </div>
+    </footer>
+      
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ template/header.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <title>CRUD php y mysql b5</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS v5.0.2 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"  integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- cdn icnonos-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+  </head>
+  <body>
+      <div class="container-fluid bg-warning">
+          <div class="row">
+              <div class="col-md">
+                  <header class="py-3">
+                      <h3 class="text-center">CRUD PHP con Bootstrap 5</h3>
+                  </header>
+              </div>
+          </div>
+      </div>
