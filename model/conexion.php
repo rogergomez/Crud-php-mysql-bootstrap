b08e1fe1efbc1f665a335085ea45b165d515324c
rogergomez <39565229+rogergomez@users.noreply.github.com>
2021-08-24 17:04:33 -0500

    Add files via upload

---
+++ model/conexion.php
@@ -0,0 +1,17 @@
+<?php 
+$contrasena = "";
+$usuario = "root";
+$nombre_bd = "crud";
+
+try {
+	$bd = new PDO (
+		'mysql:host=localhost;
+		dbname='.$nombre_bd,
+		$usuario,
+		$contrasena,
+		array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
+	);
+} catch (Exception $e) {
+	echo "Problema con la conexion: ".$e->getMessage();
+}
+?>
